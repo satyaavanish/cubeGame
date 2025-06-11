74f45c492c71be596bd8121463c52df38b613750
Satya Avanish Pulavarthi <146957189+satyaavanish@users.noreply.github.com>
2025-06-11 10:35:51 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+ import * as THREE from 'https://unpkg.com/three@0.158.0/build/three.module.js';
 import { World } from './World';
 import { createPlayer } from './Player';
 
